7cdb55338ae3051ab21895f0600e615cbd832336
yudawei1230 <374149489@qq.com>
2016-05-15 00:00:25 +0800

    dasd

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,9 @@ router.get('/', function(req, res, next) {
 });
 router.get('/wechat', function(req, res, next) {
 	var shasum = crypto.createHash('sha1');
-	var p = [req.query.token,req.query.timestamp,req.query.nonce].sort();
+	var p = ['yudawei',req.query.timestamp,req.query.nonce].sort().join('');
 	shasum.update(p);
 	var d = shasum.digest('hex');
-	console.log(d);
-	console.log(res.query.signature);
-	res.end();
-	//res.query.signature === d ? res.end(res.query.echostr) : console.log(d + '----'+res.query.signature);
+	res.query.signature === d ? res.end(res.query.echostr); : res.end();
 });
 module.exports = router;
