3a02e68e6344b29c445480b64d763a4c17664972
yudawei1230 <374149489@qq.com>
2016-05-14 23:22:57 +0800

    as

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var crypto = require('crypto');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,7 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/wechat', function(req, res, next) {
-  console.log(req.query);
-  res.end();
+	var shasum = crypto.createHash('sha1');
+	var p = [req.query.token,req.query.timestamp,req.query.nonce].sort();
+	shasum.update(p);
+	var d = shasum.digest('hex');
+	console.log(res.query.signature ===d);
+	res.query.signature ===d ? res.end(res.query.echostr) : console.log(d + '----'+res.query.signature);
 });
 module.exports = router;
