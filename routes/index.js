5ee81dd74d611ef95fbc68503ae87ad0b4e2c9e1
yudawei1230 <374149489@qq.com>
2016-05-16 16:29:28 +0800

    dsa

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 var express = require('express');
 var crypto = require('crypto');
 var router = express.Router();
+var http = require('http');
+var accessToken = '';
+var head = {
+  method:'GET',
+  host:'https://api.weixin.qq.com',
+  path:'/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET',
+  port:80,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
 
+};
+var getAccess = http.request(head,function(serverData){
+  if(serverData.satus.Code == 200){
+     serverData.on('data',function(data){
+        accessToken += data;
+     }).on('end',function(){
+       console.log(accessToken);
+       res.send(200,body);
+     })
+  }
+  else
+    res.send(500,'error');
+  console.log(accessToken);
+});
 /* GET home page. */
 router.get('/wechat', function(req, res, next) {
     console.log(req.query);
