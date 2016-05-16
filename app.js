17138b42d34d000e2eea34180e760435a55c3404
yudawei1230 <374149489@qq.com>
2016-05-16 22:04:08 +0800

    dasd

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.text({type: 'text/xml'}));
 app.use('/', routes);
 app.use('/users', users);
 
