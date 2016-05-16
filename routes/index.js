e264e77fafcf2c0d6166e7d8aa4f98565ac4da20
yudawei1230 <374149489@qq.com>
2016-05-16 16:45:28 +0800

    dasd

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var http = require('http');
 var accessToken = '';
 var head = {
   method:'GET',
-  host:'https://api.weixin.qq.com',
+  host:'api.weixin.qq.com',
+  scheme: 'https',
   path:'/cgi-bin/token?grant_type=client_credential&appid=wx958c9c637fb0e1f9&secret=e27a6f0512e827c9b9c611f6aff72188',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
